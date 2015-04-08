9dd3e446cd0f952750340ee9ead89cb6e6628317
dimanyc <dimitrynazarov@gmail.com>
2015-04-07 23:37:43 -0400

    switching to pg

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.1.6'
-gem 'sqlite3'
+gem 'pg'
 gem 'sass'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -15,3 +15,7 @@ gem 'slim'
 gem 'storytime'
 gem 'bourbon'
 gem 'neat'
+
+group :production do 
+  gem 'rails_12factor'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,64 @@
 
 ActiveRecord::Schema.define(version: 20150402040159) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: true do |t|
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "filters", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.text     "description"
+    t.text     "slips"
+    t.boolean  "case_sensitive"
+    t.boolean  "keep_slips_order"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "filters_messages", id: false, force: true do |t|
+    t.integer "filter_id",  null: false
+    t.integer "message_id", null: false
+  end
+
+  add_index "filters_messages", ["filter_id", "message_id"], name: "index_filters_messages_on_filter_id_and_message_id", using: :btree
+  add_index "filters_messages", ["message_id", "filter_id"], name: "index_filters_messages_on_message_id_and_filter_id", using: :btree
+
+  create_table "messages", force: true do |t|
+    t.text     "body"
+    t.string   "author"
+    t.string   "author_image_url"
+    t.text     "hashtags",         default: [], array: true
+    t.text     "mentions",         default: [], array: true
+    t.text     "slipped",          default: [], array: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "tweet_id"
+  end
+
+  create_table "messages_users", id: false, force: true do |t|
+    t.integer "user_id",    null: false
+    t.integer "message_id", null: false
+  end
+
+  add_index "messages_users", ["message_id", "user_id"], name: "index_messages_users_on_message_id_and_user_id", using: :btree
+  add_index "messages_users", ["user_id", "message_id"], name: "index_messages_users_on_user_id_and_message_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
   end
 
 end
