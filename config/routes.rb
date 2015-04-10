2dcda6d0169a238d88f2f9b6bebd4af71191b7ee
dimanyc <dimitrynazarov@gmail.com>
2015-04-10 02:33:10 -0400

    adding about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#design', as: 'home'
-   get '/about' => "home#about"
+  get '/about' => "home#about"
+  get '/design' => "home#design"
   resources :contacts, only:[:create]
 end
